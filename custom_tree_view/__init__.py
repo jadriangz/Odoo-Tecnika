b971ac660861ae90dd4637d0d35de7b07702d1a8
CybroOdooDev <you@example.com>
2022-09-14 11:49:52 +0530

    [updated] custom tree view

--- custom_tree_view/__init__.py
+++ custom_tree_view/__init__.py
@@ -5,45 +5,27 @@ from odoo import api, SUPERUSER_ID
 
 def _post_init_custom_tree_view(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    cr.execute(""" select account_move.id 
-        from account_move where 
-        account_move.move_type = 'out_invoice' """)
-    ac_id = cr.dictfetchall()
-    cr.execute(""" select account_account.id 
-            from account_account where 
-            account_account.internal_group = 'income' or  
-            account_account.internal_group = 'asset' """)
-    ac_name = cr.dictfetchall()
-    if ac_id and ac_name:
-        for i in ac_id:
-            for k in ac_name:
-                cr.execute(""" select account_move.id,product_template.name, 
-                        product_template.list_price,account_move_line.quantity,account_move.amount_untaxed_signed,
-                        ir_property.value_float as standard_price, 
-                        account_move.name
-                        from product_template inner join product_product
-                        on product_product.product_tmpl_id = product_template.id
-                        inner join ir_property
-                        on ir_property.res_id = 'product.product,' || product_product.id
-                        inner join account_move_line on
-                        account_move_line.product_id = product_product.id
-                        inner join account_move on
-                        account_move.id = account_move_line.move_id
-                        where account_move.move_type = 'out_invoice' and 
-                        account_move_line.account_id =%(in_ac_id)s 
-                        and account_move.id =%(ac_id)s """,
-                           {"in_ac_id": k['id'],
-                            "ac_id": i['id']})
-                data = cr.dictfetchall()
-                if data:
-                    standard_price = 0
-                    for j in data:
-                        if not j['quantity']:
-                            standard_price += j['standard_price'] * 0
-                        else:
-                            standard_price += j['standard_price'] * j[
-                                'quantity']
-                        env['account.move'].browse(int(i['id'])).write({
-                            'margin': j['amount_untaxed_signed'] - standard_price,
-                            'cost': standard_price,
-                        })
+    account_move_data = env['account.move'].search(
+        [('move_type', '=', 'out_invoice')])
+    account_account_data = env['account.account'].search(
+        ['|', ('internal_group', '=', 'income'),
+         ('internal_group', '=', 'asset')])
+    for account_move_line in account_move_data:
+        cost = 0
+        price = 0
+        margin_percentage = 0
+        for account_account_line in account_account_data:
+            account_move_line_data = env['account.move.line'].search(
+                ['&', ('move_id', '=', account_move_line.id),
+                 ('account_id', '=', account_account_line.id)])
+            if account_move_line_data:
+                for account_move_line_data_line in account_move_line_data:
+                    cost += account_move_line_data_line.product_id.standard_price * account_move_line_data_line.quantity
+        price = account_move_line.amount_untaxed_signed
+        margin = price - cost
+        if margin > 0 and price > 0:
+            margin_percentage = (margin * 100) / price
+        env['account.move'].browse(int(account_move_line['id'])).write({
+            'margin': margin,
+            'cost': cost,
+            'margin_percentage': margin_percentage})
--- custom_tree_view/models/custom_treeview.py
+++ custom_tree_view/models/custom_treeview.py
@@ -6,33 +6,42 @@ class AccountMoveInherit(models.Model):
     _inherit = 'account.move'
     cost = fields.Float(string='Cost', store="True")
     margin = fields.Float(string='Margin', store="True")
+    margin_percentage = fields.Float(string='Margin Percentage', store="True")
 
     @api.model
     def create(self, vals):
         res = super(AccountMoveInherit, self).create(vals)
         calculated_cost = 0.0
         calculated_margin = 0.0
+        calculated_subtotal = 0.0
         if res:
             for line in res.invoice_line_ids:
                 calculated_cost += line.quantity * line.product_id.standard_price
+                calculated_subtotal += line.price_subtotal
                 if line.price_unit:
                     calculated_margin += line.quantity * line.price_unit
                 else:
                     calculated_margin += line.quantity * line.product_id.list_price
+        margin = calculated_margin - calculated_cost
         res.cost = calculated_cost
-        res.margin = calculated_margin - calculated_cost
+        res.margin = margin
+        if margin > 0 and calculated_subtotal > 0:
+            res.margin_percentage = (margin * 100) / calculated_subtotal
         return res
 
     def action_post(self):
         res = super(AccountMoveInherit, self).action_post()
         calculated_cost = 0.0
         calculated_margin = 0.0
+        calculated_subtotal = 0.0
         if self:
             for line in self.invoice_line_ids:
                 calculated_cost += line.quantity * line.product_id.standard_price
+                calculated_subtotal += line.price_subtotal
                 calculated_margin += line.quantity * line.price_unit
+        margin = calculated_margin - calculated_cost
         self.cost = calculated_cost
-        self.margin = calculated_margin - calculated_cost
+        self.margin = margin
+        if margin > 0 and calculated_subtotal > 0:
+            self.margin_percentage = (margin * 100) / calculated_subtotal
         return res
-
-
