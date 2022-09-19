5e006b69cf6978c3061db145c3e801b17f23f865
CybroOdooDev <you@example.com>
2022-09-19 12:13:59 +0530

    [updated] custom payment policy

--- custom_payment_policy/models/custom_payment_policy.py
+++ custom_payment_policy/models/custom_payment_policy.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields,api
+from odoo import models, fields, api
+from datetime import datetime
 
 
 class AccountMoveInheritPaymentPolicy(models.Model):
     _inherit = 'account.move'
 
-    custom_l10n_mx_edi_payment_policy = fields.Selection(string='Payment Policy',
-                                                  selection=[('PPD', 'PPD'),
-                                                             ('PUE', 'PUE')])
+    custom_l10n_mx_edi_payment_policy = fields.Selection(
+        string='Payment Policy',
+        selection=[('PPD', 'PPD'),
+                   ('PUE', 'PUE')], required=True)
 
     @api.depends('move_type', 'company_id', 'state')
     def _compute_l10n_mx_edi_cfdi_request(self):
@@ -18,8 +20,7 @@ class AccountMoveInheritPaymentPolicy(models.Model):
                 move.l10n_mx_edi_cfdi_request = 'on_invoice'
             elif move.move_type == 'out_refund':
                 move.l10n_mx_edi_cfdi_request = 'on_refund'
-            elif (
-                    move.payment_id and move.payment_id.payment_type == 'inbound' and
+            elif (move.payment_id and move.payment_id.payment_type == 'inbound' and
                     'PPD' in move._get_reconciled_invoices().mapped(
                 'custom_l10n_mx_edi_payment_policy')):
                 move.l10n_mx_edi_cfdi_request = 'on_payment'
@@ -27,3 +28,13 @@ class AccountMoveInheritPaymentPolicy(models.Model):
                 move.l10n_mx_edi_cfdi_request = 'on_payment'
             else:
                 move.l10n_mx_edi_cfdi_request = False
+
+
+class AccountEdiFormat(models.Model):
+    _inherit = 'account.edi.format'
+
+    def _l10n_mx_edi_get_invoice_cfdi_values(self, invoice):
+        res = super(AccountEdiFormat,self)._l10n_mx_edi_get_invoice_cfdi_values(invoice)
+        res.update(
+            {'payment_policy': invoice.custom_l10n_mx_edi_payment_policy})
+        return res
